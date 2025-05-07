52f022ca3e9ef08f39564db9a728750df46bef71
Klára Ondráčková <ktsai@csob.cz>
2025-05-07 14:54:24 +0200

    ✨ Add Budget app and module

---
+++ app/src/main/java/com/example/mybudget/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.example.mybudget.di
+
+import com.example.mybudget.repository.BudgetRepository
+import com.example.mybudget.repository.BudgetRepositoryImpl
+import com.example.mybudget.ui.BudgetViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule =
+    module {
+        single<BudgetRepository> { BudgetRepositoryImpl() }
+    }
